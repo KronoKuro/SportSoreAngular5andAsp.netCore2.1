793bf06a863fdf56d0c9c92bb41c3758f9b38e39
GMCS\Pmatrosov <kurokrono@gmail.com>
2018-11-23 17:47:56 +0300

    add_card_to_products

--- WebApp/ClientApp/src/app/home/home.component.ts
+++ WebApp/ClientApp/src/app/home/home.component.ts
@@ -9,6 +9,7 @@ import { Category } from '../models/category.model';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent  {
   constructor(private productServices: ProductServices, private http: HttpClient) {
